806eb22ab394bef33973a315d9e758d3a1808ba8
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-09 22:10:46 -0500

    Update ScriptsIndex.js

---
+++ ScriptsIndex.js
@@ -0,0 +1,12 @@
+let menuInvisible=false;
+//funcion que oculta o muestra el menú
+if(menuInvisible){
+    document.getElementById("nav").classList="";
+    menuInvisible=false;
+
+}else{  
+    document.getElementById("nav").classList="responsive";
+    menuInvisible=true;
+
+
+}
